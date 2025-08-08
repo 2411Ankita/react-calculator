44b89f319873f65f704d887dad553905555d1a7e
Ankita Chirame <ankitachirame@gmail.com>
2025-08-08 06:26:02 +0530

    just do it

--- react_calculator/vite.config.js
+++ react_calculator/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/react-calculator/',
   plugins: [react()],
+  base: '/react-calculator/', // 👈 your repo name here
 })
